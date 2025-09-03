5ed5097540ef398cdce60de6ee13679f17befee9
yogeshbala <40916871+yogeshbala@users.noreply.github.com>
2025-09-03 23:44:51 +0530

    Update bollinger.py

--- indicators/bollinger.py
+++ indicators/bollinger.py
@@ -1,1 +1,15 @@
 
+import talib
+
+def compute_bollinger(df, timeperiod=20, nbdevup=2, nbdevdn=2):
+    upper, middle, lower = talib.BBANDS(
+        df['Close'],
+        timeperiod=timeperiod,
+        nbdevup=nbdevup,
+        nbdevdn=nbdevdn,
+        matype=0
+    )
+    df['BB_upper'] = upper
+    df['BB_middle'] = middle
+    df['BB_lower'] = lower
+    return df
