92a05984d0552dc1f56b1bad3f574b2788d98619
yogeshbala <40916871+yogeshbala@users.noreply.github.com>
2025-09-03 23:43:18 +0530

    Update fetch_data.py

--- data/fetch_data.py
+++ data/fetch_data.py
@@ -1,1 +1,6 @@
+import yfinance as yf
 
+def fetch_data(symbol="EURUSD=X", interval="15m", period="2d"):
+    df = yf.download(tickers=symbol, interval=interval, period=period)
+    df.dropna(inplace=True)
+    return df
