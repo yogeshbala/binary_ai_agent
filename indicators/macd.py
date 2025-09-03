4b30bd71d98998e14bcf40813eec49fbb269c21f
yogeshbala <40916871+yogeshbala@users.noreply.github.com>
2025-09-03 23:45:10 +0530

    Update macd.py

--- indicators/macd.py
+++ indicators/macd.py
@@ -1,1 +1,14 @@
 
+import talib
+
+def compute_macd(df, fastperiod=12, slowperiod=26, signalperiod=9):
+    macd, macd_signal, macd_hist = talib.MACD(
+        df['Close'],
+        fastperiod=fastperiod,
+        slowperiod=slowperiod,
+        signalperiod=signalperiod
+    )
+    df['MACD'] = macd
+    df['MACD_signal'] = macd_signal
+    df['MACD_hist'] = macd_hist
+    return df
